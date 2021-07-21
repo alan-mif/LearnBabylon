fa2dacbbb5c961d40033472a55bfc6c476d3c69d
yocover <yocover@163.com>
2021-07-21 10:22:37 +0800

    update

--- src/chap_1_firsts/01_hello_world.ts
+++ src/chap_1_firsts/01_hello_world.ts
@@ -1,32 +1,44 @@
 export function init() {
-  const canvas: HTMLCanvasElement = document.querySelector('canvas.webgl')
-  canvas.height = window.innerHeight
-  canvas.width = window.innerWidth
+    const canvas: HTMLCanvasElement = document.querySelector('canvas.webgl');
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
 
-  const engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true })
+    const engine = new BABYLON.Engine(canvas, true, {
+        preserveDrawingBuffer: true,
+        stencil: true,
+    });
 
-  const createScene = () => {
-    const scene = new BABYLON.Scene(engine)
+    const createScene = () => {
+        const scene = new BABYLON.Scene(engine);
 
-    const camera = new BABYLON.ArcRotateCamera('camera', -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0), scene)
-    camera.attachControl(canvas, true)
+        const camera = new BABYLON.ArcRotateCamera(
+            'camera',
+            -Math.PI / 2,
+            Math.PI / 2.5,
+            3,
+            new BABYLON.Vector3(0, 0, 0),
+            scene
+        );
+        camera.attachControl(canvas, true);
 
-    const light1 = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene)
+        const light1 = new BABYLON.HemisphericLight(
+            'light1',
+            new BABYLON.Vector3(0, 1, 0),
+            scene
+        );
 
-    const box = BABYLON.MeshBuilder.CreateBox('box', {})
+        const box = BABYLON.MeshBuilder.CreateBox('box', {});
 
-    return scene
+        return scene;
+    };
 
-  }
+    const scene = createScene();
 
-  const scene = createScene()
+    engine.runRenderLoop(() => {
+        scene.render();
+    });
 
-  engine.runRenderLoop(() => {
-    scene.render()
-  })
-
-  window.addEventListener('resize', () => {
-    engine.resize()
-  })
-
+    window.addEventListener('resize', () => {
+        engine.resize();
+    });
 }
