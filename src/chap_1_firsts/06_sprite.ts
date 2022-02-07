7d16184463d964886bc7ecb10ea975edf3d99f80
alan-mif <949031267@qq.com>
2022-02-07 09:44:04 +0800

    add

---
+++ src/chap_1_firsts/06_sprite.ts
@@ -0,0 +1,39 @@
+export class Sprite {
+
+    /**
+     * 构造函数
+     */
+    public constructor() {
+        this._init();
+    }
+
+    private _init(): void {
+
+        const canvas: HTMLCanvasElement = document.querySelector('canvas.webgl');
+        canvas.height = window.innerHeight;
+        canvas.width = window.innerWidth;
+
+        const engine = new BABYLON.Engine(canvas, true), // 参数1传入 canvas， 参数2指定是否开启抗锯齿
+            scene = new BABYLON.Scene(engine),
+            camera = new BABYLON.ArcRotateCamera('camera', -Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 0, 0), scene); // 参数1定义相机的 name，参数2定义相机沿y轴的旋转弧度，参数3定义相机沿x轴的旋转弧度，参数4定义相机与目标的距离，参数5定义相机的目标，参数6定义相机所属的场景
+        camera.attachControl(canvas, true);
+
+        new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 1), scene); // 参数1灯光 name，参数2灯光位置，参数3灯光所在的场景
+
+        const spriteManager = new BABYLON.SpriteManager("SpriteManager", "./textures/player.png", 2000, { width: 64, height: 64 }, scene);
+        const sprite = new BABYLON.Sprite("sprite", spriteManager);
+        sprite.cellIndex = 0; // 指定精灵图（cellIndex在精灵表中的计数是从左到右，自上而下的）
+        sprite.playAnimation(0, 43, true, 100);
+        const box = BABYLON.MeshBuilder.CreateBox('box', { height: 1, width: 1, depth: 1 }, scene); // 为了和对比精灵
+        box.position.y = -1; // 调整盒子坐标，避免挡住精灵图
+
+        engine.runRenderLoop((): void => {
+            scene.render();
+        });
+
+        window.addEventListener('resize', (): void => {
+            engine.resize();
+        });
+
+    }
+}
---
+++ src/chap_1_firsts/Base.ts
@@ -0,0 +1,102 @@
+import { Listener } from "./Listener";
+
+export interface Size {
+    height: number;
+    width: number;
+    depth: number;
+}
+
+export interface Mesh {
+    content: BABYLON.Mesh;
+    size: Size;
+    direction: BABYLON.Vector3;
+}
+
+export interface Sprite {
+    content: BABYLON.Sprite;
+    time: number;
+    velocity: BABYLON.Vector3;
+    isStop: boolean;
+}
+
+export class Base {
+
+    /** 画布 */
+    public canvas: HTMLCanvasElement;
+    /** 引擎 */
+    public engine: BABYLON.Engine;
+    /** 场景 */
+    public scene: BABYLON.Scene;
+    /** 相机 */
+    public camera: BABYLON.ArcRotateCamera | BABYLON.UniversalCamera;
+    /** 物体集合 */
+    public meshes: Array<Mesh> = [];
+    /** 精灵集合 */
+    public sprites: Array<Sprite> = [];
+    /** 地板 */
+    public ground: BABYLON.Mesh;
+
+    /** 当前时间 */
+    protected _currentTime: number = new Date().getTime();
+    /** 开始时间 */
+    protected _startTime: number = new Date().getTime();
+    /** 监听器 */
+    protected _listener: Listener;
+
+    /**
+     * 构造函数
+     */
+    public constructor() {
+
+        this._init();
+
+    }
+
+    /**
+     * 初始化
+     */
+    protected _init(): void {
+
+        this.canvas = document.querySelector('canvas.webgl');
+        this.canvas.height = window.innerHeight;
+        this.canvas.width = window.innerWidth;
+
+        this.engine = new BABYLON.Engine(this.canvas, true);
+        this.scene = new BABYLON.Scene(this.engine);
+        this.camera = new BABYLON.ArcRotateCamera('camera', -Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 0, 0), this.scene);
+        this.ground = BABYLON.MeshBuilder.CreateGround("myGround", { width: 50, height: 50, subdivisions: 4 });
+
+        const groundMaterial: BABYLON.StandardMaterial = new BABYLON.StandardMaterial("material", this.scene);
+        groundMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+        this.ground.material = groundMaterial;
+
+        this.camera.attachControl(this.canvas, true); // 相机绑定控制
+        new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), this.scene); // 添加半球光用来模拟环境光
+
+        // 事件监听
+        this._listener = new Listener(this.canvas);
+        this._listener.addResize((): void => this.engine.resize());
+
+    }
+
+    /**
+     * 创建天空盒
+     * @param src 资源路径
+     */
+    protected _createSkybox(src: string) {
+
+        const skyBox: BABYLON.Mesh = BABYLON.MeshBuilder.CreateBox("skyBox", {
+            size: 2000.0
+        }, this.scene),
+            skyBoxMaterial = new BABYLON.StandardMaterial("skyBox", this.scene);
+
+        skyBoxMaterial.backFaceCulling = false;
+        skyBoxMaterial.reflectionTexture = new BABYLON.CubeTexture(src, this.scene);
+        skyBoxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+        skyBoxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+        skyBoxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+        skyBox.material = skyBoxMaterial;
+
+    }
+
+}
