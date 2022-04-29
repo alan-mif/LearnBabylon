d68552633701a8c0d0bbf741d4e5597762c1a4a1
alan-mif <949031267@qq.com>
2022-04-29 17:09:44 +0800

    Lack of discrete processing

--- src/chap_1_firsts/10_collideGame.ts
+++ src/chap_1_firsts/10_collideGame.ts
@@ -6,12 +6,20 @@ export class CollideGame extends Base {
     /** 主角 */
     public protagonist: BABYLON.Mesh;
 
-    /** x 限制 */
+    /** x+ 限制 */
     private _limitX: boolean;
-    /** y 限制 */
+    /** y+ 限制 */
     private _limitY: Boolean;
-    /** z 限制 */
+    /** z+ 限制 */
     private _limitZ: boolean;
+    /** x- 限制 */
+    private _limitXN: boolean;
+    /** y- 限制 */
+    private _limitYN: Boolean;
+    /** z- 限制 */
+    private _limitZN: boolean;
+    /** 碰撞物体 */
+    private _collideMeshes: BABYLON.Mesh[] = [];
 
     /**
      * 构造函数
@@ -74,26 +82,135 @@ export class CollideGame extends Base {
 
     }
 
+    private _isContain(id: string) {
+
+        for (let i = 0; i < this._collideMeshes.length; i++) {
+
+            if (this._collideMeshes[i].id === id) {
+                return true;
+            }
+
+        }
+
+        return false;
+
+    }
+
     /**
      * 碰撞检测
      */
     private _collisionCheck(x: number, y: number, z: number): void {
 
-        const protagonist = this.protagonist;
+        const protagonist: BABYLON.Mesh = this.protagonist;
 
         protagonist.position.x += x;
-        protagonist.position.y += y;
-        protagonist.position.z += z;
 
-        for (let i = 0; i < this.meshes.length; i++)  if (protagonist.intersectsMesh(this.meshes[i].content, false)) this._limitX = true;
+        for (let i = 0; i < this.meshes.length; i++) {
 
-        for (let i = 0; i < this.meshes.length; i++)  if (protagonist.intersectsMesh(this.meshes[i].content, false)) this._limitY = true;
+            const mesh: BABYLON.Mesh = this.meshes[i].content;
 
-        for (let i = 0; i < this.meshes.length; i++)  if (protagonist.intersectsMesh(this.meshes[i].content, false)) this._limitZ = true;
+            if (this._isContain(mesh.id)) continue;
+
+            if (protagonist.intersectsMesh(mesh, false)) {
+
+                if (x > 0) {
+                    this._limitX = true;
+                } else {
+                    this._limitXN = true;
+                }
+
+                mesh.checkCollisions = true;
+                this._collideMeshes.push(mesh);
+
+            }
+
+        }
 
         protagonist.position.x -= x;
-        protagonist.position.y -= y;
+        protagonist.position.z += z;
+
+        for (let i = 0; i < this.meshes.length; i++) {
+
+            const mesh: BABYLON.Mesh = this.meshes[i].content;
+
+            if (this._isContain(mesh.id) && !mesh.checkCollisions) continue;
+
+            if (protagonist.intersectsMesh(mesh, false)) {
+
+                if (z > 0) {
+                    this._limitZ = true;
+                } else {
+                    this._limitZN = true;
+                }
+
+                if (!this._isContain(mesh.id)) this._collideMeshes.push(mesh);
+
+            }
+
+        }
+
+        protagonist.position.x += x;
+
+        let flag: boolean = false;
+
+        for (let i = 0; i < this.meshes.length; i++) {
+
+            const mesh: BABYLON.Mesh = this.meshes[i].content;
+
+            if (this._isContain(mesh.id) && !mesh.checkCollisions) continue;
+
+            if (protagonist.intersectsMesh(mesh, false)) {
+
+                flag = true;
+                if (!this._isContain(mesh.id)) this._collideMeshes.push(mesh);
+
+            }
+
+        }
+
+        if (flag) {
+
+            if (!this._limitX && !this._limitXN! && !this._limitZ && !this._limitZN) {
+
+                if (z > 0) {
+                    this._limitZ = true;
+                } else {
+                    this._limitZN = true;
+                }
+
+            }
+
+        }
+
+        protagonist.position.x -= x;
         protagonist.position.z -= z;
+
+        for (let i = 0; i < this._collideMeshes.length; i++) {
+            this._collideMeshes[i].checkCollisions = false;
+        }
+
+    }
+
+    /**
+     * 分离检查
+     */
+    private _separateCheck(): void {
+
+        let flag: boolean = false;
+
+        for (let i = this._collideMeshes.length - 1; i > -1; i--) {
+
+            if (this.protagonist.intersectsMesh(this._collideMeshes[i], false)) {
+                flag = true;
+            } else {
+                this._collideMeshes.pop();
+            }
+
+        }
+
+        if (!flag) {
+            this._limitX = this._limitXN = this._limitY = this._limitYN = this._limitZ = this._limitZN = false;
+        }
 
     }
 
@@ -107,19 +224,19 @@ export class CollideGame extends Base {
 
         this._collisionCheck(x, y, z);
 
-        if (!this._limitX) {
+        if ((!this._limitX && x > 0) || (!this._limitXN && x < 0)) {
             this.camera.position.x += x, this.protagonist.position.x += x;
         }
 
-        if (!this._limitY) {
+        if ((!this._limitY && y > 0) || (!this._limitYN && y < 0)) {
             this.camera.position.y += y, this.protagonist.position.y += y;
         }
 
-        if (!this._limitZ) {
+        if ((!this._limitZ && z > 0) || (!this._limitZN && z < 0)) {
             this.camera.position.z += z, this.protagonist.position.z += z;
         }
 
-        this._limitX = this._limitY = this._limitZ = false;
+        this._separateCheck();
 
     }
 
